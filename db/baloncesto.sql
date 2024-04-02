1c5c993943755f52b93df297802a1901ee9b5931
Omaraitlhouss1 <159189285+Omaraitlhouss1@users.noreply.github.com>
2024-04-02 16:31:51 +0200

    Creado trabajo despliegue automático en Azure

--- db/baloncesto.sql
+++ db/baloncesto.sql
@@ -3,12 +3,12 @@ CREATE DATABASE baloncesto;
 USE baloncesto;
 
 CREATE TABLE `Jugadores` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `nombre` VARCHAR(50) NOT NULL DEFAULT '',
-    `votos` INT NOT NULL DEFAULT 0,
-    PRIMARY KEY (`id`),
-    UNIQUE INDEX `UNIQUE_KEY` (`id`)
-)  ENGINE=INNODB DEFAULT CHARSET=UTF8 COLLATE = UTF8_GENERAL_CI;
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`nombre` VARCHAR(50) NOT NULL DEFAULT '',
+	`votos` INT NOT NULL DEFAULT 0, 
+	PRIMARY KEY (`id`),
+	UNIQUE INDEX `UNIQUE_KEY` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 
 INSERT INTO Jugadores (id, nombre, votos) VALUES (1, 'Llull', 0);
